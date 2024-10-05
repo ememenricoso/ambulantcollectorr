363d68aa96a2523fcb05ecffdbb7b094403af7c4
Fatima Enricoso <fatimaenricoso2112@gmail.com>
2024-10-05 23:08:33 +0800

    main

--- lib/screens/Tuesday.dart
+++ lib/screens/Tuesday.dart
@@ -8,10 +8,34 @@ class TuesdayPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tuesday Assignments'),
-        backgroundColor: Colors.green,
-      ),
+             appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white), // Back icon with white color
+            onPressed: () {
+              Navigator.of(context).pop(); // Navigate back to the previous screen
+            },
+          ),
+          title: const Text(""), // Empty title to avoid spacing issues
+          flexibleSpace: const Center( // Center the content
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center, // Center the text and icon
+              mainAxisSize: MainAxisSize.min, // Minimize the space taken by the Row
+              children: [
+                Icon(Icons.check_circle, color: Colors.white), // Icon next to the text
+                SizedBox(width: 8), // Space between icon and text
+                Text(
+                  "Tuesday Vendors",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20, // Set text color to white
+                  ),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: Colors.green, // Set background color to green
+          elevation: 1.0,
+        ),
       body: _buildTuesdayVendorList(context),
     );
   }
